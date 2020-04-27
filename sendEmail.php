2244b251be3540808f642d9a7f568fe09be7b35a
Jusan Magno Pinto da Silva <jusanmagno@gmail.com>
2020-04-27 12:59:27 -0300

    inicio do projeto

---
+++ sendEmail.php
@@ -0,0 +1,24 @@
+<?php
+
+ini_set('display_errors', 1);
+
+error_reporting(E_ALL);
+
+$from = $_POST['email'];
+
+$to = "jusanmagno@gmail.com";
+
+$subject = "Enviado do Site de " . $_POST['nome'];
+
+$message = "De: " . $_POST['nome'] . " \n" . " Telefone: " . $_POST['tel'] . " \n" . " mensagem: " . $_POST['msn'];
+
+$headers = "De:" . $from;
+
+if (mail($to, $subject, $message, $headers)) {
+
+    echo "A mensagem de e-mail foi enviada.";
+    $_SESSION['msg'] = 'Mensagem enviada com sucesso';
+    header('Location: contact.html');
+} else {
+    $_SESSION['msg'] = 'há um erro ao enviar mensagem. Tente mais tarde';
+}
